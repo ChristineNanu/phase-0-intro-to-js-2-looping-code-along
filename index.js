3c923c52906e88d5615b0649b375bec19d4136f9
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-03-20 01:53:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+}
+  
+    function writeCards(names) {
+        const messages = names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+        return messages;
+      }
+      
+      
+      const names = ['Alice', 'Bob', 'Charlie'];
+      const thankYouMessages = writeCards(names);
+      console.log(thankYouMessages);
+    
